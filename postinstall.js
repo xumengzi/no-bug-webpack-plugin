60931d82524fb6ef92c13f27f44338833bc44e9d
xumeng17 <xumeng17@meituan.com>
2022-05-19 19:43:54 +0800

    fix: 增加版本

--- postinstall.js
+++ postinstall.js
@@ -3,23 +3,40 @@ var os = require('os');
 var path = require('path');
 var env = process.env;
 
+// 包的名字
 var packageName = env.npm_package_name;
+// 包的版本
 var version = env.npm_package_version;
+// 项目路径，比如：/Users/xumeng/Meituan/grocery-mux-product
+var INIT_CWD = env.INIT_CWD;
+// npm的声明周期，详情：https://docs.npmjs.com/cli/v8/using-npm/scripts
+var npm_lifecycle_event = env.npm_lifecycle_event
 
-var BANNER = '\u001B[96mwarning：您当前升级的版本存在break-change，请关注升级文档 (\u001B[94m https://km.sankuai.com/page/1316040376 \u001B[96m) 获取更多信息!\u001B[0m\n';
+var prevVersion = '';
+var currVersion = '';
 
-function isBannerRequired() {
-    console.log(env);
-    if (packageName === 'mux-ui') {
-        
+var tips = '';
+
+// 获取安装前的版本号
+if (npm_lifecycle_event === 'preinstall') {
+    prevVersion = version;
+}
+
+// 获取安装后的版本号
+if (npm_lifecycle_event === 'postinstall') {
+    currVersion = version;
+}
+
+console.log('prevVersion', prevVersion);
+console.log('currVersion', currVersion);
+
+function isShowTips() {
+    if (prevVersion && currVersion){
+        if (prevVersion.substring(0,3) !== currVersion.substring(0,3)) {
+            tips = '\u001B[96mwarning：您当前升级的版本存在break-change，请关注升级文档 (\u001B[94m https://km.sankuai.com/page/1316040376 \u001B[96m) 获取更多信息!\u001B[0m\n';
+            console.log(tips.replace(/\u001B\[\d+m/g, ''));
+        }
     }
-    return true;
 }
 
-function showBanner() {
-    console.log('升级拉');
-  // eslint-disable-next-line no-console, regexp/no-control-character -- output
-  console.log(COLOR ? BANNER : BANNER.replace(/\u001B\[\d+m/g, ''));
-}
-
-if (isBannerRequired()) showBanner();
+isShowTips();
