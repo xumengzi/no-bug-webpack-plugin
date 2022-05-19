01865749ca120eb846b06f381b0cb939969a2a44
xumeng17 <xumeng17@meituan.com>
2022-05-19 20:41:13 +0800

    fix: 增加版本

--- postinstall.js
+++ postinstall.js
@@ -19,17 +19,32 @@ var tips = '';
 // 获取安装后的版本号 0.1.0  0.2.0 0.3.1
 if (npm_lifecycle_event === 'postinstall') {
     currVersion = version;
-    if (currVersion.substring(currVersion.length - 1) === '0') {
+    const versionObj = getChangeList();
+    if (versionObj && Object.keys(versionObj).length) {
+        if (versionObj[currVersion]) {
+            isShowTips(versionObj[currVersion]);
+        }
+    } else if (currVersion.substring(currVersion.length - 1) === '0') {
         isShowTips();
     }
 }
 
-console.log('prevVersion', prevVersion);
-console.log('currVersion', currVersion);
-
-function isShowTips() {
-    tips = '\u001B[96mwarning：您当前升级的版本存在break-change，请关注升级文档 (\u001B[94m https://km.sankuai.com/page/1316040376 \u001B[96m) 获取更多信息!\u001B[0m\n';
+function isShowTips(description = '改动较多，请注意') {
+    tips = `
+            \u001B[96mwarning：您当前升级的版本${currVersion}存在break-change：${description}，
+            请关注升级文档 (\u001B[94m https://km.sankuai.com/page/1316040376 \u001B[96m) 获取更多信息!\u001B[0m\n
+        `;
     console.log(tips.replace(/\u001B\[\d+m/g, ''));
 }
 
-
+function getChangeList() {
+    try {
+        let changeObj = fs.readFileSync('./breakchange.json', 'utf8')
+        changeObj = JSON.parse(changeObj);
+        
+        return changeObj;
+    } catch (err) {
+        console.error(err)
+        return null;
+    }
+}
