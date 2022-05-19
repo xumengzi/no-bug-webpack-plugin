3aa8cdbf7616d2fd1c45ffdcf10cda156762cf10
xumeng17 <xumeng17@meituan.com>
2022-05-19 19:27:00 +0800

    fix: 增加版本

--- postinstall.js
+++ postinstall.js
@@ -9,12 +9,15 @@ var version = env.npm_package_version;
 var BANNER = '\u001B[96mwarning：您当前升级的版本存在break-change，请关注升级文档 (\u001B[94m https://km.sankuai.com/page/1316040376 \u001B[96m) 获取更多信息!\u001B[0m\n';
 
 function isBannerRequired() {
-    console.log(packageName, version);
-  if (packageName === 'mux-ui') {}
-  return true;
+    console.log(env);
+    if (packageName === 'mux-ui') {
+        
+    }
+    return true;
 }
 
 function showBanner() {
+    console.log('升级拉');
   // eslint-disable-next-line no-console, regexp/no-control-character -- output
   console.log(COLOR ? BANNER : BANNER.replace(/\u001B\[\d+m/g, ''));
 }
